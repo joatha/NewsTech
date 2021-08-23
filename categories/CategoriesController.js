4c8399f34227104a80c7901062d54c5683608d01
Joathã Andrade <joathaandrade2@gmail.com>
2021-08-23 18:27:49 -0300

    Atualizando

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const Category = require("./Category")
+const slugify = require("slugify")
 
-router.get("/categorias", (req, res)=>{
-    res.send("Testando Aqui se roda!")
+
+
+router.get("/admin/categories/new", (req, res)=>{
+    res.render("admin/categories/new")
 })
 
+router.post("/categories/save", (req, res)=>{
+    var title = req.body.title
 
+    if(title != undefined){
+
+        Category.create({
+            title: title,
+            slug:slugify(title)
+        }).then(()=>{
+            res.redirect("/")
+        }).catch((err)=>{
+            console.log("Erro ao cadastrar no banco", err)
+        })
+
+    }else{
+        res.redirect("/admin/categories/new")
+    }
+
+})
 module.exports = router
