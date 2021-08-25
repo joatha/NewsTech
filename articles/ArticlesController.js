66dc77c5a7c22e0194712db819dbf9b30b789796
Joathã Andrade <joathaandrade2@gmail.com>
2021-08-25 09:44:09 -0300

    Implementando novas funcionalidades

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const Category = require("../categories/Category")
+const Article = require("./Article")
+const slugify = require("slugify")
 
-router.get("/articles", (req, res)=>{
-    res.send("Testando de artigo")
+router.get("/admin/articles", (req, res)=>{
+
+    Article.findAll({
+        include:[{model: Category}]
+    }).then(articles =>{
+        res.render("admin/articles/index",{articles:articles })
+    })
+
 })
 
 
@@ -13,6 +21,21 @@ router.get("/admin/articles/new", (req, res)=>{
 
         res.render("admin/articles/new",{categories: categories})
 
+    })
+})
+
+router.post("/articles/save",(req,res)=>{
+    let title = req.body.title
+    let body = req.body.body
+    let category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect("/admin/articles")
     })
 })
 
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 
 const conexaoComBanco = new Sequelize('newstech', 'root','joatha', {
     host: 'localhost',
-    dialect:'mysql'
+    dialect:'mysql',
+    timezone:"-03:00"
 })
 
 module.exports = conexaoComBanco
